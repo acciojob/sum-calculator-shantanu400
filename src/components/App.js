b12bdf53823bd35d633dfc49201902dd37bb4740
shantanu400 <ashunayan400@gmail.com>
2024-07-15 12:27:57 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
+import { TextField,Typography} from "@material-ui/core";
+
 
 const App = () => {
+  const [val,setval]=useState(0);
+  const [sum,setsum]=useState(0);
+  useEffect(()=>{
+    
+    setsum(sum+Number(val));
+        
+  
+    }, [val]);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <TextField type="number" InputLabelProps={{ shrink: true }} onChange={(e)=>{
+        setval(e.target.value);
+        
+      }} />
+        <Typography>{sum}</Typography>   
     </div>
   )
 }
